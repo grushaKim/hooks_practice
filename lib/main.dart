4fc6bc77af88b3ebc05e0a0dab91b885cf4e3d60
grushaKim <grushenka.k18@gmail.com>
2023-02-03 18:02:15 +0900

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
+
+void testIt() {
+  final values = [1, 2, null, 3];
+  final nonNullValues = values.compactMap((e) {
+    if (e != null && e > 10) {
+      return e;
+    } else {
+      return null;
+    }
+  });
+}
 
 void main() => runApp(const App());
 
@@ -18,20 +39,22 @@ class App extends StatelessWidget {
   }
 }
 
+const url =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmMGZnowGWA2mhq9SEa3jpvGgWOiKfLM-eug&usqp=CAU";
+
 class HomeScreen extends HookWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState(''); // Value notifier
+    final future = useMemoized(() {
+      NetworkAssetBundle(Uri.parse(url))
+          .load(url)
+          .then((data) => data.buffer.asUint8List())
+          .then((data) => Image.memory(data));
+    });
 
-    useEffect(() {
-      controller.addListener(() {
-        text.value = controller.text;
-      });
-      return null;
-    }, [controller]); // controller as a key
+    final snapshot = useFuture(future);
 
     return Scaffold(
       appBar: AppBar(
@@ -39,12 +62,9 @@ class HomeScreen extends HookWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(20, 100, 20, 0),
-        child: Column(children: [
-          TextFormField(
-            controller: controller,
-          ),
-          Text('You typed - ${text.value}'),
-        ]),
+        child: Column(
+          children: <Widget>[snapshot.data].compactMap().toList(),
+        ),
       ),
     );
   }
